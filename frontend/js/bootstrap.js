c94cb576bbd0720ff1e669fc837b628ebb5f41ab
albert.denova <albert.denova@uols.org>
2015-05-17 12:34:10 +0200

    Created project and added first physics blocks

---
+++ frontend/js/bootstrap.js
@@ -0,0 +1,18 @@
+var ioSocket = io.connect();
+var game = new Phaser.Game(
+    800, 
+    800, 
+    Phaser.AUTO, 
+    '', 
+    { 
+        preload: function() {
+                        
+        }, 
+        create: function() {
+            
+        },
+        update: function() {
+            
+        }
+    }
+);
